1774aa19347e4de3fb85f9b593e94569fffffc21
Prashant Pokhriyal <prashant.pokhriyal@vedantu.com>
2021-03-03 23:13:18 +0530

    dynamically show card logo based on number

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -3,25 +3,84 @@ import { useState, useEffect } from 'react';
 import { Card, Row, Col, Image } from 'react-bootstrap';
 import './index.scss';
 import chip from '../../assets/img/chip.png';
-import vendor from '../../assets/img/visa.png';
+import amex from '../../assets/img/amex.png';
+import dinersclub from '../../assets/img/dinersclub.png';
+import discover from '../../assets/img/discover.png';
+import jcb from '../../assets/img/jcb.png';
+import mastercard from '../../assets/img/mastercard.png';
+import troy from '../../assets/img/troy.png';
+import unionpay from '../../assets/img/unionpay.png';
+import visa from '../../assets/img/visa.png';
 
 export default function CreditCard(props) {
     const [state, setState] = useState({});
+    const [vendor, setVendor] = useState(null);
+
+    const regexMapping = [
+        {
+            image: amex,
+            regex: /^(3[47])\d+$/,
+        },
+        {
+            image: dinersclub,
+            regex: /^3(?:0[0-5]|[68][0-9])\d+$/,
+        },
+        {
+            image: discover,
+            regex: /^6\d+$/,
+        },
+        {
+            image: jcb,
+            regex: /^(2131|1800|35)\d*$/
+        },
+        {
+            image: mastercard,
+            regex: /^(5[1-5][0-9])\d+$/,
+        },
+        {
+            image: troy,
+            regex: /^(9792)\d*$/,
+        },
+        {
+            image: unionpay,
+            regex: /^(62|88)\d+$/,
+        },
+        {
+            image: visa,
+            regex: /^(4)\d+$/,
+        },
+    ];
+
+    const getCardType = (number) => {
+        for (let card of regexMapping) {
+            if (card.regex.test(number)) {
+                return card.image;
+            }
+        }
+
+        return regexMapping[regexMapping.length - 1].image;
+    };
 
     useEffect(() => {
         setState({ ...props });
         console.log(props);
     }, [props]);
 
+    useEffect(() => {
+        debugger;
+        if (state.number)
+            setVendor(getCardType(state.number[0]));
+    }, [state.number]);
+
     return (
         <div className={`cc-container ${props.className}`}>
             <div className={state.showFront ? 'card-flip' : `card-flip hover`}>
                 <div className="flip">
                     <div className="front">
-                        <CardFront {...state} />
+                        <CardFront {...state} vendor={vendor} />
                     </div>
                     <div className="back">
-                        <CardBack {...state} />
+                        <CardBack {...state} vendor={vendor} />
 
                     </div>
                 </div>
@@ -39,11 +98,11 @@ const CardFront = (state) => {
                         <Image className="cc-chip" src={chip} fuild={true} />
                     </Col>
                     <Col className="text-right">
-                        <Image className="cc-vendor-name" src={vendor} fuild={true} />
+                        <Image key={state.vendor} className="cc-vendor-name slide-down" src={state.vendor} fuild={true} />
                     </Col>
                 </Row>
                 <Row className="cc-number mt-5 font-weight-bold">
-                    {
+                  {
                         state.number?.map(chunk => (
                             <Col>
                                 {
@@ -94,7 +153,7 @@ const CardBack = (state) => {
                 </Row>
                 <Row className="mt-4">
                     <Col className="text-right">
-                        <Image className="cc-vendor-name" src={vendor} fuild={true} />
+                        <Image className="cc-vendor-name" src={state.vendor} fuild={true} />
                     </Col>
                 </Row>
             </Card.Body>
